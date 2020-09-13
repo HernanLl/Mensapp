a41feb8234a3096206df23862a4a9682497489df
Hernan Llull <hernanllull@gmail.com>
2020-09-13 17:15:49 -0300

    -

--- Server/src/config/google_auth.js
+++ Server/src/config/google_auth.js
@@ -1,7 +1,6 @@
 const credentials = {
   web: {
-    client_id:
-      "484660170455-voth5f1a06jbl8lecnljfg581h0rjko2.apps.googleusercontent.com",
+    client_id: process.env.CLIENT_ID,
     client_secret: process.env.CLIENT_SECRET,
   },
 };
@@ -9,7 +8,7 @@ const credentials = {
 const tokens = {
   access_token: process.env.ACCESS_TOKEN,
   refresh_token: process.env.REFRESH_TOKEN,
-  expiry_date: 1587509950237,
+  expiry_date: process.env.EXPIRE,
 };
 
 module.exports = { credentials, tokens };
